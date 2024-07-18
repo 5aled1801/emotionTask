70238cca377b44683346d0d90964443f8ac3fe3a
Khaled Hassan <zxkmkm432184@gmail.com>
2024-07-18 15:37:22 +0300

    add more items

---
+++ src/components/navBar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="position-fixed right-0 h-screen top-0 pa-5" style="background-color: aliceblue">
+        <v-btn variant="text" color="green">
+            <v-badge color="error" content="2">
+                <v-icon style="font-size: 30px;">mdi-cart</v-icon>
+            </v-badge>
+        </v-btn>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,82 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+import navBar from '../components/navBar.vue'
+export default {
+  data() {
+    return {
+      All: true,
+      Food: false,
+      NonFood: false,
+      Fashone: false
+    }
+  },
+  components: {
+    navBar
+  }
+}
 </script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <v-container fluid class="top-0 position-absolute left-0">
+      <navBar />
+      <v-row>
+        <v-col cols="11" class="d-flex justify-end">
+          <v-btn
+            variant="text"
+            :color="All ? 'orange' : 'gray'"
+            @click="
+              (this.All = !this.All) &&
+                (this.Food = false) &&
+                (this.Fashone = false) &&
+                (this.NonFood = false)
+            "
+            >All</v-btn
+          >
+          <v-btn
+            variant="text"
+            :color="Food ? 'orange' : 'gray'"
+            @click="
+              (this.Food = !this.Food) &&
+                (this.Fashone = false) &&
+                (this.NonFood = false) &&
+                (this.All = false)
+            "
+            >Food</v-btn
+          >
+          <v-btn
+            variant="text"
+            :color="NonFood ? 'orange' : 'gray'"
+            @click="
+              (this.Food = false) &&
+                (this.Fashone = false) &&
+                (this.NonFood = this.NonFood) &&
+                (this.All = false)
+            "
+            >Non Food</v-btn
+          >
+          <v-btn
+            variant="text"
+            :color="Fashone ? 'orange' : 'gray'"
+            @click="
+              (this.Food = false) &&
+                (this.Fashone = !this.Fashone) &&
+                (this.NonFood = false) &&
+                (this.All = false)
+            "
+          >
+            Fashon & Linen</v-btn
+          >
+        </v-col>
+      </v-row>
+
+      
+      <v-row>
+        <v-col cols="6" class="d-flex justify-center">
+          <!-- {{ items.length }} -->
+          <span> products</span>
+          <v-btn variant="text"><v-icon>mdi-table</v-icon></v-btn
+          ><v-btn variant="text"><v-icon>mdi-table</v-icon></v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
